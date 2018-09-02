88614784e4f60da87e95c1748705a3e94dfb8b44
dhiraoka <demo.sert@gmail.com>
2018-09-02 15:17:08 +0900

    Categoryを持つように追加

--- models/history.go
+++ models/history.go
@@ -2,8 +2,9 @@ package models
 
 type History struct {
 	Base
-	Name       string `json:"name" gorm:"not null"`
-	Amount     int    `json:"amount" gorm:"not null"`
-	CategoryID uint   `json:"category_id" gorm:"not null"`
-	AssetID    uint   `json:"asset_id" gorm:"not null"`
+	Name       string   `json:"name" gorm:"not null"`
+	Amount     int      `json:"amount" gorm:"not null"`
+	CategoryID uint     `json:"category_id" gorm:"not null"`
+	AssetID    uint     `json:"asset_id" gorm:"not null"`
+	Category   Category `json:"category"`
 }
